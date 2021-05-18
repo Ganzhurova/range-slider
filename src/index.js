c42420a1bc5c7ea28173c656a32564a8caab7108
Darima Ganzhurova <DGanzurova@gmail.com>
2021-05-18 16:26:19 +0900

    Инициализация slider1 - для одиночного слайдера, slider2 - для двойного

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import './range-slider.scss';
 import './style.scss';
 import RangeSlider from './range-slider';
 
-const slider1 = new RangeSlider();
+const slider1 = new RangeSlider('#slider-1', {});
+
+const slider2 = new RangeSlider('#slider-2', {
+  type: 'double',
+});
 
 console.log(slider1);
+console.log(slider2);
