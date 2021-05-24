12e920fc639c9ceb30472629250c17ee424cc900
Darima Ganzhurova <DGanzurova@gmail.com>
2021-05-24 22:25:02 +0900

    Добавлен класс Model

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import './range-slider.scss';
 import './style.scss';
-import RangeSlider from './range-slider';
 
-const slider1 = new RangeSlider('#slider-1', {});
+import Model from './Model/Model';
+// import RangeSlider from './range-slider';
+//
+// const slider1 = new RangeSlider('#slider-1', {});
+//
+// const slider2 = new RangeSlider('#slider-2', {
+//   type: 'double',
+// });
+//
+// const slider3 = new RangeSlider('#slider-3', {
+//   isVertical: true,
+// });
+//
+// console.log(slider1);
+// console.log(slider2);
+// console.log(slider3);
 
-const slider2 = new RangeSlider('#slider-2', {
+const model = new Model({
   type: 'double',
 });
-
-console.log(slider1);
-console.log(slider2);
+console.log(model);
