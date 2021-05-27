1a6fe6307adb23a04a85a48404e1659fc8483da7
Darima Ganzhurova <DGanzurova@gmail.com>
2021-05-27 21:42:23 +0900

    Создан модуль для хранения html атрибутов

---
+++ src/ts/View/HTMLDefaults.js
@@ -0,0 +1,13 @@
+const rootClass = 'range-slider';
+
+const HTMLDefaults = {
+  rootClass,
+  directionModifier: 'vertical',
+
+  line: {
+    className: `${rootClass}__line`,
+    tag: 'div',
+  },
+};
+
+export default HTMLDefaults;
