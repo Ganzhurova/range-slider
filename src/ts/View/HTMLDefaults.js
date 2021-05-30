e53c2b320b86ef8ce17f97afffe571f8cb1b181b
Darima Ganzhurova <DGanzurova@gmail.com>
2021-05-30 20:58:17 +0900

    Текущая работа над переменными для имен классов

--- src/ts/View/HTMLDefaults.js
+++ src/ts/View/HTMLDefaults.js
@@ -1,13 +1,23 @@
 const rootClass = 'range-slider';
 
 const HTMLDefaults = {
-  rootClass,
-  directionModifier: 'vertical',
-
-  line: {
-    className: `${rootClass}__line`,
-    tag: 'div',
+  modifiers: {
+    direction: 'vertical',
+    from: 'from',
+    to: 'to',
   },
+
+  rootClass,
+  // directionModifier: 'vertical',
+  line: `${rootClass}__line`,
+  bar: `${rootClass}__bar`,
+  thumb: `${rootClass}__thumb`,
+  label: `${rootClass}__label`,
+
+  // line: {
+  //   className: `${rootClass}__line`,
+  //   tag: 'div',
+  // },
 };
 
 export default HTMLDefaults;
