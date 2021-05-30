a7d5225a33d21b1e8b50a83e8d850c6d6e75f0c6
Darima Ganzhurova <DGanzurova@gmail.com>
2021-05-30 21:02:13 +0900

    Рефакторинг

--- src/ts/View/subViews/LineView.js
+++ src/ts/View/subViews/LineView.js
@@ -1,6 +1,11 @@
-// import HTMLDefaults from '../HTMLDefaults';
+import HTMLDefaults from '../HTMLDefaults';
 import HTMLElement from '../HTMLElement';
 
-class LineView extends HTMLElement {}
+class LineView extends HTMLElement {
+  constructor(options) {
+    super(options);
+    this.createEl({ className: `${HTMLDefaults.rootClass}__line` });
+  }
+}
 
 export default LineView;
