db22f9afc84103df85f3c008f7582093373f29d6
Darima Ganzhurova <DGanzurova@gmail.com>
2021-05-30 22:41:36 +0900

    Добавлены опции для вертикального слайдера, удален закомментированный код

--- src/index.js
+++ src/index.js
@@ -4,27 +4,18 @@ import './style.scss';
 import Model from './range-slider/Model/Model';
 import View from './range-slider/View/View';
 import Presenter from './range-slider/Presenter/Presenter';
-// import RangeSlider from './range-slider';
-//
-// const slider1 = new RangeSlider('#slider-1', {});
-//
-// const slider2 = new RangeSlider('#slider-2', {
-//   type: 'double',
-// });
-//
-// const slider3 = new RangeSlider('#slider-3', {
-//   isVertical: true,
-// });
-//
-// console.log(slider1);
-// console.log(slider2);
-// console.log(slider3);
 
-const options = {
+const options1 = {
   type: 'double',
-  // vertical: true,
   label: true,
-  // scale: true,
+  scale: true,
 };
 
-console.log(new Presenter(new View('#slider-1'), new Model(options)));
+const options3 = {
+  vertical: true,
+  label: true,
+  scale: true,
+};
+
+console.log(new Presenter(new View('#slider-1'), new Model(options1)));
+console.log(new Presenter(new View('#slider-3'), new Model(options3)));
