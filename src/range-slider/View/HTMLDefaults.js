822f003c3a9e4c0e7052dabddcd8d02b10abe27d
Darima Ganzhurova <DGanzurova@gmail.com>
2021-05-30 22:42:58 +0900

    Удалены ненужные переменные

--- src/range-slider/View/HTMLDefaults.js
+++ src/range-slider/View/HTMLDefaults.js
@@ -1,23 +1,6 @@
-const rootClass = 'range-slider';
-
 const HTMLDefaults = {
-  modifiers: {
-    direction: 'vertical',
-    from: 'from',
-    to: 'to',
-  },
-
-  rootClass,
-  // directionModifier: 'vertical',
-  line: `${rootClass}__line`,
-  bar: `${rootClass}__bar`,
-  thumb: `${rootClass}__thumb`,
-  label: `${rootClass}__label`,
-
-  // line: {
-  //   className: `${rootClass}__line`,
-  //   tag: 'div',
-  // },
+  rootClass: 'range-slider',
+  directionModifier: 'vertical',
 };
 
 export default HTMLDefaults;
