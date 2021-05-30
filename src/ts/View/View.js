aa417e1244f2d97955614bc5b08dc8495a4d7021
Darima Ganzhurova <DGanzurova@gmail.com>
2021-05-30 21:00:39 +0900

    Работа над методами инициализации View и рендера элементов

--- src/ts/View/View.js
+++ src/ts/View/View.js
@@ -1,7 +1,12 @@
 import EventEmitter from '../EventEmitter';
 import HTMLDefaults from './HTMLDefaults';
+// import types from '../defaults';
+// import Template from './Template';
 
 import LineView from './subViews/LineView';
+import BarView from './subViews/BarView';
+import ThumbView from './subViews/ThumbView';
+import LabelView from './subViews/LabelView';
 
 class View extends EventEmitter {
   constructor(selector) {
@@ -9,10 +14,41 @@ class View extends EventEmitter {
     this.el = document.querySelector(selector);
   }
 
-  init(options) {
+  init(viewModel) {
+    this.line = new LineView(viewModel);
+    this.bar = new BarView(viewModel);
+    this.thumb = new ThumbView(viewModel);
+    this.label = new LabelView(viewModel);
+    console.log(this.line);
+    console.log(this.bar);
+    console.log(this.thumb);
+    console.log(this.label);
+
+    this.elements = {
+      line: this.line.getEl(),
+      bar: this.bar.getEl(),
+      thumbs: this.thumb.getEl(),
+      labels: this.label.getEl(),
+    };
+
+    console.log(this.elements);
+    // const template = new Template(viewModel).init();
+    // this.el.innerHTML = template;
+
     this.el.classList.add(HTMLDefaults.rootClass);
-    this.#setDirection(options.isVertical);
+    this.#setDirection(viewModel.isVertical);
+
     this.#render();
+  }
+
+  #render() {
+    this.elements.line.append(
+      this.elements.bar,
+      this.elements.thumbs,
+      this.elements.labels ?? ''
+    );
+
+    this.el.append(this.elements.line);
   }
 
   #setDirection(isVertical) {
@@ -21,12 +57,6 @@ class View extends EventEmitter {
     } else {
       this.el.classList.remove(HTMLDefaults.directionModifier);
     }
-  }
-
-  #render() {
-    this.line = new LineView();
-
-    console.log(this.line);
   }
 }
 
