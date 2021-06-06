e17fe7b6e9baa94afadbb619be1a023cc6771107
Darima Ganzhurova <DGanzurova@gmail.com>
2021-06-06 14:40:48 +0900

    Добавлены значения по умолчанию для опций

--- src/range-slider/defaults.js
+++ src/range-slider/defaults.js
@@ -3,4 +3,16 @@ const types = {
   DOUBLE: 'double',
 };
 
-export default types;
+const defaults = {
+  type: types.SINGLE,
+  vertical: false,
+  label: false,
+  scale: false,
+  min: 0,
+  max: 100,
+  from: null,
+  to: null,
+  step: 1,
+};
+
+export { types, defaults };
