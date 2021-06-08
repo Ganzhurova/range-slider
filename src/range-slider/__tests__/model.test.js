9c1f6fc9f84040d4fe50987135dec6fd8bba6524
Darima Ganzhurova <DGanzurova@gmail.com>
2021-06-08 22:02:55 +0900

    Создан файл для тестирования Model

---
+++ src/range-slider/__tests__/model.test.js
@@ -0,0 +1,9 @@
+import Model from '../Model/Model';
+
+describe('Model: setType', () => {
+  const model = new Model();
+
+  test('should be defined', () => {
+    expect(model.setType).toBeDefined();
+  });
+});
