9113bfbcc024fc9be3e3611a80365612070425c7
Darima Ganzhurova <DGanzurova@gmail.com>
2021-06-09 18:39:25 +0900

    Добавлены тесты для методов get/set Vertical, Label, Scale

--- src/range-slider/__tests__/model.test.js
+++ src/range-slider/__tests__/model.test.js
@@ -2,30 +2,15 @@ import Model from '../Model/Model';
 
 describe('Model: setType', () => {
   const model = new Model();
-  const { type } = model.settings;
 
   test.each([
-    { value: 1, expected: type },
-    { value: true, expected: type },
-    { value: 'string', expected: type },
-  ])(
-    'the type value should not change. Add invalid value $value',
-    ({ value, expected }) => {
-      const spy = jest.spyOn(model, 'setType');
-      model.setType(value);
-
-      expect(spy).toHaveBeenCalled();
-      expect(model.settings.type).toBe(expected);
-
-      spy.mockRestore();
-    }
-  );
-
-  test.each([
+    { value: 1, expected: 'single' },
+    { value: true, expected: 'single' },
+    { value: 'string', expected: 'single' },
     { value: 'double', expected: 'double' },
     { value: 'single', expected: 'single' },
   ])(
-    'must change the type. Add a valid value $value',
+    'type must have the correct value("double" or "single"). Add value $value',
     ({ value, expected }) => {
       const spy = jest.spyOn(model, 'setType');
       model.setType(value);
