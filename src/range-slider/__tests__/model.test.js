6acf0f785660a5d3b1e5bb9235a0f3036aea90f8
Darima Ganzhurova <DGanzurova@gmail.com>
2021-06-09 16:06:15 +0900

    Добавлен тест для метода setType

--- src/range-slider/__tests__/model.test.js
+++ src/range-slider/__tests__/model.test.js
@@ -2,8 +2,38 @@ import Model from '../Model/Model';
 
 describe('Model: setType', () => {
   const model = new Model();
+  const { type } = model.settings;
 
-  test('should be defined', () => {
-    expect(model.setType).toBeDefined();
-  });
+  test.each([
+    { value: 1, expected: type },
+    { value: true, expected: type },
+    { value: 'string', expected: type },
+  ])(
+    'the type value should not change. Add invalid value $value',
+    ({ value, expected }) => {
+      const spy = jest.spyOn(model, 'setType');
+      model.setType(value);
+
+      expect(spy).toHaveBeenCalled();
+      expect(model.settings.type).toBe(expected);
+
+      spy.mockRestore();
+    }
+  );
+
+  test.each([
+    { value: 'double', expected: 'double' },
+    { value: 'single', expected: 'single' },
+  ])(
+    'must change the type. Add a valid value $value',
+    ({ value, expected }) => {
+      const spy = jest.spyOn(model, 'setType');
+      model.setType(value);
+
+      expect(spy).toHaveBeenCalled();
+      expect(model.settings.type).toBe(expected);
+
+      spy.mockRestore();
+    }
+  );
 });
