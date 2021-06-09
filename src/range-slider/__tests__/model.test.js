a6deed173f0bb7e555db3a7e8123db848c1a7d84
Darima Ganzhurova <DGanzurova@gmail.com>
2021-06-09 18:12:47 +0900

    Добавлены тесты для метода setVertical

--- src/range-slider/__tests__/model.test.js
+++ src/range-slider/__tests__/model.test.js
@@ -49,5 +49,53 @@ describe('Model: getType', () => {
 
     expect(spy).toHaveBeenCalled();
     expect(type).toBe(originalType);
+
+    spy.mockRestore();
+  });
+});
+
+describe('Model: setVertical', () => {
+  const model = new Model();
+  const { vertical } = model.settings;
+
+  test.each([
+    { value: 1, expected: vertical },
+    { value: 'string', expected: vertical },
+  ])(
+    'the vertical value should not change. Add invalid value $value',
+    ({ value, expected }) => {
+      const spy = jest.spyOn(model, 'setVertical');
+      model.setVertical(value);
+
+      expect(spy).toHaveBeenCalled();
+      expect(model.settings.vertical).toBe(expected);
+
+      spy.mockRestore();
+    }
+  );
+
+  test.each([
+    { value: true, expected: true },
+    { value: false, expected: false },
+  ])(
+    'must change the vertical. Add a valid value $value',
+    ({ value, expected }) => {
+      const spy = jest.spyOn(model, 'setVertical');
+      model.setVertical(value);
+
+      expect(spy).toHaveBeenCalled();
+      expect(model.settings.vertical).toBe(expected);
+
+      spy.mockRestore();
+    }
+  );
+
+  test('isBoolean method must be called', () => {
+    const isBooleanSpy = jest.spyOn(Model, 'isBoolean');
+    const arg = true;
+    model.setVertical(arg);
+
+    expect(isBooleanSpy).toHaveBeenCalled();
+    expect(isBooleanSpy).toHaveBeenCalledWith(arg);
   });
 });
