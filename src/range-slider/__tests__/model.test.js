2256cf37767bd78909054f8bd84f5f8c50280999
Darima Ganzhurova <DGanzurova@gmail.com>
2021-06-12 21:54:59 +0900

    Добавлены тесты для методов setStep и setFrom, начальное тестирование update

--- src/range-slider/__tests__/model.test.js
+++ src/range-slider/__tests__/model.test.js
@@ -2,6 +2,63 @@ import Model from '../Model/Model';
 
 // const descOnly = describe.only;
 
+describe('Model: update', () => {
+  let model;
+
+  beforeEach(() => {
+    model = new Model();
+  });
+
+  test.each([
+    {
+      options: undefined,
+      expected: {
+        type: 'single',
+        vertical: false,
+        label: false,
+        scale: false,
+        min: 0,
+        max: 100,
+        from: null,
+        to: null,
+        step: 1,
+      },
+    },
+    {
+      options: {
+        type: 'double',
+        vertical: true,
+        label: true,
+        scale: true,
+        min: '6000',
+        max: 158.9,
+      },
+      expected: {
+        type: 'double',
+        vertical: true,
+        label: true,
+        scale: true,
+        min: 0,
+        max: 159,
+        from: null,
+        to: null,
+        step: 1,
+      },
+    },
+  ])(
+    'must update the current state. Add options: $options',
+    ({ options, expected }) => {
+      const spy = jest.spyOn(model, 'update');
+      model.update(options);
+
+      expect(spy).toHaveBeenCalled();
+      expect(model.state).toEqual(expected);
+
+      spy.mockRestore();
+    }
+  );
+});
+
 describe('Model: setType', () => {
   const model = new Model();
 
@@ -190,6 +247,113 @@ describe('Model: setLimits', () => {
 
       expect(spy).toHaveBeenCalled();
       expect(limitsAfterCall).toEqual(expected);
+
+      spy.mockRestore();
+    }
+  );
+});
+
+describe('Model: setStep', () => {
+  let model;
+  const defaultValue = 1;
+
+  beforeEach(() => {
+    model = new Model();
+  });
+
+  test.each([
+    { value: 'one', expected: defaultValue },
+    { value: NaN, expected: defaultValue },
+    { value: undefined, expected: defaultValue },
+    { value: 2.5, expected: 3 },
+    { value: 40, expected: 40 },
+  ])('must assign a valid value. Add value $value', ({ value, expected }) => {
+    const spy = jest.spyOn(model, 'setStep');
+    model.setStep(value);
+
+    expect(spy).toHaveBeenCalled();
+    expect(model.state.step).toBe(expected);
+
+    spy.mockRestore();
+  });
+
+  test.each([
+    { value: -10, expected: defaultValue },
+    { value: 0, expected: defaultValue },
+    { value: 150, expected: defaultValue },
+  ])('must be a positive number. Add value $value', ({ value, expected }) => {
+    const spy = jest.spyOn(model, 'setStep');
+    model.setStep(value);
+
+    expect(spy).toHaveBeenCalled();
+    expect(model.state.step).toBe(expected);
+
+    spy.mockRestore();
+  });
+
+  test('must be less than the max value', () => {
+    const spy = jest.spyOn(model, 'setStep');
+    const max = model.getMax();
+
+    model.setStep(60);
+
+    expect(spy).toHaveBeenCalled();
+    expect(model.state.step).toBeLessThan(max);
+
+    spy.mockRestore();
+  });
+});
+
+describe('Model: setFrom', () => {
+  let model;
+  const defaultValue = null;
+
+  beforeEach(() => {
+    model = new Model();
+  });
+
+  test.each([
+    { value: 'one', expected: defaultValue },
+    { value: NaN, expected: defaultValue },
+    { value: undefined, expected: defaultValue },
+    { value: 2.5, expected: 3 },
+    { value: 40, expected: 40 },
+  ])('must assign a valid value. Add value $value', ({ value, expected }) => {
+    const spy = jest.spyOn(model, 'setFrom');
+    model.setFrom(value);
+
+    expect(spy).toHaveBeenCalled();
+    expect(model.state.from).toBe(expected);
+
+    spy.mockRestore();
+  });
+
+  test('value must be in the range between min and max', () => {
+    const spy = jest.spyOn(model, 'setFrom');
+    const max = model.getMax();
+    const min = model.getMin();
+
+    model.setFrom(60);
+
+    expect(spy).toHaveBeenCalled();
+    expect(model.state.from).toBeLessThanOrEqual(max);
+    expect(model.state.from).toBeGreaterThanOrEqual(min);
+
+    spy.mockRestore();
+  });
+
+  test.each([
+    { value: 40, type: 'double', expected: defaultValue },
+    { value: 40, type: 'single', expected: 40 },
+  ])(
+    'must assign a value only for a single type. Add type $type',
+    ({ value, type, expected }) => {
+      const spy = jest.spyOn(model, 'setFrom');
+      model.setType(type);
+      model.setFrom(value);
+
+      expect(spy).toHaveBeenCalled();
+      expect(model.state.from).toBe(expected);
 
       spy.mockRestore();
     }
