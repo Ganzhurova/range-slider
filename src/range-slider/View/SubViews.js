d3595c1d685a5ed42f6a78c7cac657098a6e7224
Darima Ganzhurova <DGanzurova@gmail.com>
2021-06-21 15:55:33 +0900

    Создан реэкспорт для subViews

---
+++ src/range-slider/View/SubViews.js
@@ -0,0 +1,5 @@
+import LineView from './subViews/LineView';
+import BarView from './subViews/BarView';
+import ThumbView from './subViews/ThumbView';
+
+export { LineView as line, BarView as bar, ThumbView as thumb };
