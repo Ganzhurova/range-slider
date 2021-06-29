3e7091636782be21bfac5e519ea4fee4a56c3c27
Darima Ganzhurova <DGanzurova@gmail.com>
2021-06-29 23:42:39 +0900

    Удалены

--- src/range-slider/View/SubViews.js
+++
@@ -1,5 +0,0 @@
-import LineView from './subViews/LineView';
-import BarView from './subViews/BarView';
-import ThumbView from './subViews/ThumbView';
-
-export { LineView as line, BarView as bar, ThumbView as thumbs };
