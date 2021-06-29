be712b73117c51b858db499a383e55c945d3f160
Darima Ganzhurova <DGanzurova@gmail.com>
2021-06-29 23:47:39 +0900

    RootView переписан, оставлен один метод, устанавливающий направление слайдера, вызывается модулем Template

--- src/range-slider/View/subViews/RootView.js
+++ src/range-slider/View/subViews/RootView.js
@@ -1,59 +1,19 @@
-// import vDom from '../vDom';
-import { directionMix } from '../../lib/html';
+import Component from './Component';
+import { html } from '../../lib/html';
 
-class RootView {
-  constructor(html) {
-    this.options = {};
-
-    this.tag = '';
-    this.attrs = {};
-    this.children = [];
+class RootView extends Component {
+  constructor() {
+    super();
 
     this.init(html.rootEl);
   }
 
-  toggleClassInAttr(className, boolean) {
-    const separator = ' ';
-    let classes = this.attrs.class.split(separator);
-    const isClassMissing = cls => cls !== className;
-    const requiredClass = () => isClassMissing;
-
-    if (boolean && classes.every(isClassMissing)) {
-      classes.push(className);
+  setDirection(className, boolean) {
+    if (boolean) {
+      this.addClass(className);
     } else {
-      classes = classes.filter(requiredClass);
+      this.removeClass(className);
     }
-
-    this.attrs.class = classes.join(separator);
-  }
-
-  init(html) {
-    // убрать в компонент
-    this.tag = html.tag;
-    this.attrs = {
-      class: html.className,
-    };
-  }
-
-  setDirection() {
-    this.toggleClassInAttr(directionMix, this.options.isVertical);
-  }
-
-  update(options) {
-    if (this.options.isVertical === options.isVertical) return;
-    this.options.isVertical = options.isVertical; // частичный интерфейс?
-
-    this.setDirection();
-  }
-
-  getVNode() {
-    // return vDom.createVNode.call(this, this.tag, this.attrs, this.children);
-    const { tag, attrs, children } = this;
-    return {
-      tag,
-      attrs,
-      children,
-    };
   }
 }
 
