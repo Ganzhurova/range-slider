e690646db7299cdcf72b0b4ecacbfdc8b4a58fdf
Darima Ganzhurova <DGanzurova@gmail.com>
2021-07-14 14:43:21 +0900

    Метод getCoords перенесен в Component

--- src/range-slider/View/subViews/LineView.js
+++ src/range-slider/View/subViews/LineView.js
@@ -1,22 +1,10 @@
 import Component from '../Component';
 import { html } from '../../lib/html';
-import { size } from '../../lib/constants';
 
 class LineView extends Component {
   constructor() {
     super();
     this.init(html.line);
-  }
-
-  getCoords(isVertical) {
-    const lineBox = this.el.getBox();
-    const sizeName = isVertical ? size.HEIGHT : size.WIDTH;
-
-    return {
-      left: lineBox.left + window.pageXOffset,
-      top: lineBox.top + window.pageYOffset,
-      [sizeName]: lineBox[sizeName],
-    };
   }
 }
 
