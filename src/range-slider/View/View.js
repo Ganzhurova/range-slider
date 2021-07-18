11b913b381bcd6b99a119dc3ca10dd1c33fb5248
Darima Ganzhurova <DGanzurova@gmail.com>
2021-07-18 21:32:58 +0900

    Добавлены методы для определения предельных координат, передачи новых позиций в presenter, установки и снятия класса selected

--- src/range-slider/View/View.js
+++ src/range-slider/View/View.js
@@ -1,8 +1,7 @@
 import EventEmitter from '../EventEmitter';
 import Template from './Template';
-// import ThumbView from './subViews/ThumbView';
-import { html } from '../lib/html';
-import { positionIndex } from '../lib/constants';
+import { html, mix } from '../lib/html';
+import helpers from '../helpers/helpers';
 
 class View extends EventEmitter {
   constructor(selector) {
@@ -28,49 +27,62 @@ class View extends EventEmitter {
     Object.assign(this, subViews);
   }
 
-  getPosition(index) {
-    const positionName = positionIndex[index];
-    return this.options[positionName];
+  correctPosition(position) {
+    return position - this.options.min;
   }
 
-  updatePosition() {
+  getCorrectPosition(index) {
+    const positionName = helpers.getPositionName(index);
+    return this.correctPosition(this.options[positionName]);
+  }
+
+  setPosition() {
     const range = Math.abs(this.options.max - this.options.min);
-    const correctPosition = position => position - this.options.min;
+    const limitCoords = {};
+    const addCoord = (pxValue, index) => {
+      limitCoords[index] = pxValue;
+    };
+
+    const updatePosition = (pxValue, index) => {
+      this.updatePosition(pxValue, index);
+    };
+
+    const calcLimitCoords = (pxValue, index) => {
+      addCoord(pxValue, index);
+      this.calcLimitCoords(limitCoords, index);
+    };
 
     this.thumbs.forEach((thumb, i) => {
+      const position = this.getCorrectPosition(i);
+
       thumb.calcUnit(this.line.getSize(), range);
-      thumb.setup(correctPosition(this.getPosition(i)));
+      thumb.unsubscribe('valueChanged', updatePosition);
+      thumb.unsubscribe('valueChanged', calcLimitCoords);
+      thumb.subscribe('valueChanged', updatePosition);
+      thumb.subscribe('valueChanged', calcLimitCoords);
+      console.log(thumb);
+      thumb.setup(position);
     });
+  }
+
+  updatePosition(pxValue, index) {
+    const correctPosition = position => position + this.options.min;
+    const position = correctPosition(
+      this.thumbs[index].pxValueToPosition(pxValue)
+    );
+
+    this.emit('positionChanged', position, index);
+  }
+
+  calcLimitCoords(coords, index) {
+    this.thumbs[index].calcLimitCoords(coords);
   }
 
   update(options) {
     this.template.build(options);
 
-    this.options = { ...options };
-    this.updatePosition();
-  }
-
-  getLimitCoords(i) {
-    const limitCoords = { ...this.lineCoords };
-
-    if (this.options.isDouble) {
-      if (i === 0) {
-        limitCoords.start = this.lineCoords.start;
-        limitCoords.end =
-          this.thumbs[1].getPxValue() - this.thumbs[1].el.offsetWidth;
-        console.log(limitCoords);
-        return limitCoords;
-      }
-      if (i === 1) {
-        limitCoords.start =
-          this.thumbs[0].getPxValue() + this.thumbs[0].el.offsetWidth;
-        limitCoords.end = this.lineCoords.end;
-        console.log(limitCoords);
-        return limitCoords;
-      }
-    }
-
-    return limitCoords;
+    this.options = options;
+    this.setPosition();
   }
 
   setHandlers() {
@@ -83,11 +95,16 @@ class View extends EventEmitter {
     e.preventDefault();
     if (!e.target.classList.contains(html.thumb.className)) return;
 
-    const i = [...e.target.parentElement.children].indexOf(e.target);
-    this.thumbs[i].handlerThumbDragStart(this.line.getCoords(), e);
-    const pxValue = this.thumbs[i].getPxValue();
-    console.log(pxValue);
-    // this.thumbs[i].emit('pxValueChanged');
+    const index = [...e.target.parentElement.children].indexOf(e.target);
+
+    this.thumbs.forEach((thumb, i) => {
+      if (index === i) {
+        this.thumbs[i].handlerThumbDragStart(this.line.getCoords(), e);
+        this.thumbs[i].addClass(mix.selected);
+      } else {
+        this.thumbs[i].removeClass(mix.selected);
+      }
+    });
   }
 }
 
