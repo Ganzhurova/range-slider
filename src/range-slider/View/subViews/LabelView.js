8ba7b9b6d09dcf0324766bbc282c207398f512b3
Darima Ganzhurova <DGanzurova@gmail.com>
2021-07-27 20:37:09 +0900

    Удален метод setIndex

--- src/range-slider/View/subViews/LabelView.js
+++ src/range-slider/View/subViews/LabelView.js
@@ -5,13 +5,8 @@ import { html } from '../../lib/html';
 class LabelView extends Component {
   constructor() {
     super();
-    this.index = 0;
 
     this.init(html.label);
-  }
-
-  setIndex(index) {
-    helpers.setIndex.call(this, index);
   }
 
   correctDirection() {
