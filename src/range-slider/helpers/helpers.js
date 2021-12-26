1d0dbbd04606d7d75eb5eaf0670901950fd4b481
Darima Ganzhurova <DGanzurova@gmail.com>
2021-12-26 15:41:15 +0800

    Добавена функция для создания элемента

--- src/range-slider/helpers/helpers.js
+++ src/range-slider/helpers/helpers.js
@@ -16,5 +16,11 @@ const helpers = {
   getEvent(event) {
     return event.type.search('touch') !== -1 ? event.touches[0] : event;
   },
+
+  createEl(html) {
+    const el = document.createElement(html.tag);
+    el.className = html.className;
+    return el;
+  },
 };
 export default helpers;
