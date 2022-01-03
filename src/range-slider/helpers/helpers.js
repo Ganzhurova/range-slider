fd0b3c69c689686a7f51122dcf353196cbc31901
Darima Ganzhurova <DGanzurova@gmail.com>
2022-01-03 15:59:14 +0800

    Добавлен метод для определения главной координаты события

--- src/range-slider/helpers/helpers.js
+++ src/range-slider/helpers/helpers.js
@@ -9,6 +9,12 @@ const helpers = {
     return event.type.search('touch') !== -1 ? event.touches[0] : event;
   },
 
+  getEventCoord(event, instance) {
+    const evt = helpers.getEvent(event);
+    const pageCoordName = `page${instance.coordName}`;
+    return evt[pageCoordName];
+  },
+
   createEl(html) {
     const el = document.createElement(html.tag);
     el.className = html.className;
