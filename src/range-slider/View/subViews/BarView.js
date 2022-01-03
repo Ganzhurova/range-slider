38d8bbf6ab1c4a95ea6fd0597f6c5056c367adc0
Darima Ganzhurova <DGanzurova@gmail.com>
2022-01-03 17:47:20 +0800

    Переименование px в проценты

--- src/range-slider/View/subViews/BarView.js
+++ src/range-slider/View/subViews/BarView.js
@@ -9,22 +9,22 @@ class BarView extends Component {
     this.init(html.bar);
   }
 
-  setup(correctValue, pxValues) {
+  setup(correctValue, percentValues) {
     this.fixStyle(size, BarView.sizeName);
     this.fixStyle(directions, BarView.direction);
 
     const START_POSITION = 0;
-    const isSingleType = Object.keys(pxValues).length === 1;
-    const fromPos = pxValues[0] + correctValue;
-    const toPos = pxValues[1] - pxValues[0];
+    const isSingleType = Object.keys(percentValues).length === 1;
+    const fromPos = percentValues[0] + correctValue;
+    const toPos = percentValues[1] - percentValues[0];
 
     this.el.style[BarView.direction] = isSingleType
-      ? `${START_POSITION}px`
-      : `${fromPos}px`;
+      ? `${START_POSITION}%`
+      : `${fromPos}%`;
 
     this.el.style[BarView.sizeName] = isSingleType
-      ? `${fromPos}px`
-      : `${toPos}px`;
+      ? `${fromPos}%`
+      : `${toPos}%`;
   }
 }
 
