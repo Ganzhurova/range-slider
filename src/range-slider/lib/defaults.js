1b228c07d0211e8e81c2254b64f90e3228b0edf7
Darima Ganzhurova <DGanzurova@gmail.com>
2022-01-04 13:50:49 +0800

    Добавлена переменная для количества частей, на которое делится шкала

--- src/range-slider/lib/defaults.js
+++ src/range-slider/lib/defaults.js
@@ -7,6 +7,7 @@ const defaults = {
   isScale: false,
   min: 0,
   max: 100,
+  scaleParts: 4,
   // scaleRange: [], // вынести в интерфейс
   // isDouble: false, // вынести в интерфейс для вида
 };
