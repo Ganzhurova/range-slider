302e826bb2aa8d93495ccf65b4cda9167a541d3c
Darima Ganzhurova <DGanzurova@gmail.com>
2022-01-04 23:17:37 +0800

    Текущая работа над методом для определения перекрытия делений шкалы друг другом

--- src/range-slider/View/subViews/ScaleView.js
+++ src/range-slider/View/subViews/ScaleView.js
@@ -1,55 +1,79 @@
 import Component from '../Component';
+import ScaleDivisionView from './ScaleDivisionView';
 import { html } from '../../lib/html';
-import helpers from '../../helpers/helpers';
+// import helpers from '../../helpers/helpers';
 import { size } from '../../lib/constants';
 
 class ScaleView extends Component {
   constructor() {
     super();
     this.init(html.scale);
+    this.divisions = [];
   }
 
   setup(sizeValue, values) {
-    this.values = values;
     this.fixStyle(size, ScaleView.sizeName);
     this.el.innerHTML = '';
     this.updateSize(sizeValue);
 
     this.renderDivisions(values);
+    this.setVisibilityOfValues();
   }
 
   renderDivisions(values) {
     const step = ScaleView.getPercentageStep(values);
 
     values.forEach((value, i) => {
-      const division = ScaleView.createDivision(value);
-      division.style[ScaleView.direction] = `${step * i}%`;
-      this.el.append(division);
+      const division = new ScaleDivisionView();
+      division.setup(value);
+      division.el.style[ScaleView.direction] = `${step * i}%`;
+      this.addChild(division);
+      this.divisions.push(division);
     });
   }
 
   updateSize(sizeValue) {
-    this.el.style[ScaleView.sizeName] = `${sizeValue}px`;
+    this.el.style[ScaleView.sizeName] = `${sizeValue}%`;
   }
 
-  getValue(e) {
-    return this.values.indexOf(e.target);
+  getOverlapDivisions() {
+    const sumDivisionsSize = this.divisions
+      .map(division => division.getSize())
+      .reduce((total, amount) => total + amount);
+
+    const scaleSize = this.getSize();
+
+    if (scaleSize > sumDivisionsSize) {
+      return false;
+    }
+
+    return true;
   }
+
+  setVisibilityOfValues() {
+    const isOverlap = this.getOverlapDivisions();
+    if (!isOverlap) return;
+    console.log(isOverlap);
+  }
+
+  // getValue(e) {
+  //   return this.values.indexOf(e.target);
+  // }
 
   static getPercentageStep(values) {
     return 100 / (values.length - 1);
   }
 
-  static createDivision(valueText) {
-    const division = helpers.createEl(html.scaleDivision);
-    const point = helpers.createEl(html.scalePoint);
-    const value = helpers.createEl(html.scaleValue);
-
-    value.textContent = valueText;
-
-    division.append(point, value);
-    return division;
-  }
+  // static createDivision(valueText) {
+  //   const division = helpers.createEl(html.scaleDivision);
+  //   const point = helpers.createEl(html.scalePoint);
+  //   const value = helpers.createEl(html.scaleValue);
+  //
+  //   value.textContent = valueText;
+  //
+  //   division.append(point, value);
+  //   return division;
+  // }
 }
 
 export default ScaleView;
