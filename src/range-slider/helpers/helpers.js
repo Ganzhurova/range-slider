a138e83dedbd553e6e924bf86fd538cdf503bf75
Darima Ganzhurova <DGanzurova@gmail.com>
2022-01-08 20:21:27 +0800

    Добавлен метод определения ближайшего значения

--- src/range-slider/helpers/helpers.js
+++ src/range-slider/helpers/helpers.js
@@ -14,5 +14,11 @@ const helpers = {
     const pageCoordName = `page${instance.coordName}`;
     return evt[pageCoordName];
   },
+
+  getClosestValue(arrValues, targetValue) {
+    return arrValues.reduce((prev, curr) =>
+      Math.abs(curr - targetValue) < Math.abs(prev - targetValue) ? curr : prev
+    );
+  },
 };
 export default helpers;
