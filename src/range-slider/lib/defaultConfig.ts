15e40eae89e7ad1ea396e8bea99754945c587a5c
Darima Ganzhurova <DGanzurova@gmail.com>
2022-03-01 11:45:22 +0800

    Добавлены опции

--- src/range-slider/lib/defaultConfig.ts
+++ src/range-slider/lib/defaultConfig.ts
@@ -1,15 +1,17 @@
-import type { IOptions } from './interfaces';
+import IOptions from './interfaces';
 
 const DEFAULT_CONFIG: IOptions = {
-  type: 'single',
+  isDouble: false,
   isVertical: false,
   isLabel: false,
   isScale: false,
   min: 0,
   max: 100,
   scaleParts: 4,
+  from: 25,
+  to: 75,
+  step: 0,
   // scaleRange: [], // вынести в интерфейс
-  // isDouble: false, // вынести в интерфейс для вида
 };
 
 export default DEFAULT_CONFIG;
