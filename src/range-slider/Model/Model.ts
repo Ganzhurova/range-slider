4fca817fe2c5bb04cec928e7879de7c267db521a
Darima Ganzhurova <DGanzurova@gmail.com>
2022-03-01 13:09:08 +0800

    Model объединена с объектом State, переписана на ts

---
+++ src/range-slider/Model/Model.ts
@@ -0,0 +1,83 @@
+import IOptions from '../lib/interfaces';
+import DEFAULT_CONFIG from '../lib/defaultConfig';
+
+class Model {
+  private state: IOptions = DEFAULT_CONFIG;
+
+  constructor(options: Partial<IOptions>) {
+    this.updateState(options);
+
+    console.log(this.state);
+  }
+
+  public updateState(options: Partial<IOptions>): void {
+    Object.assign(this.state, options);
+    this.validate();
+  }
+
+  public getState(): IOptions {
+    return this.state;
+  }
+
+  private validate(): void {
+    this.validateLimits();
+    this.validatePos();
+    this.validateStep();
+    this.validateScaleParts();
+  }
+
+  private validateLimits(): void {
+    let { min, max } = this.state;
+
+    if (min === max) {
+      max += 1;
+    }
+
+    if (min > max) {
+      [min, max] = [max, min];
+    }
+
+    this.state.min = min;
+    this.state.max = max;
+  }
+
+  private validatePos(): void {
+    let { from, to } = this.state;
+
+    const isInRange = (value: number): boolean =>
+      value >= this.state.min && value <= this.state.max;
+
+    const isInvalidComparison = (): boolean => this.state.isDouble && from > to;
+
+    const getDefaultFrom = (): number => this.state.min;
+    const getDefaultTo = (): number => this.state.max;
+
+    if (!isInRange(from)) {
+      from = getDefaultFrom();
+    }
+
+    if (!isInRange(to)) {
+      to = getDefaultTo();
+    }
+
+    if (isInvalidComparison()) {
+      [from, to] = [to, from];
+    }
+
+    this.state.from = from;
+    this.state.to = to;
+  }
+
+  private validateStep(): void {
+    const { step } = this.state;
+    this.state.step = Math.abs(step);
+  }
+
+  private validateScaleParts(): void {
+    let parts: number = this.state.scaleParts;
+    parts = Number.isInteger(parts) ? parts : Math.round(parts);
+    this.state.scaleParts = Math.abs(parts);
+  }
+}
+
+export default Model;
