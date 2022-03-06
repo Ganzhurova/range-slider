78408cd8caca08d19dd28a0dfd9a8c58decbb61d
Darima Ganzhurova <DGanzurova@gmail.com>
2022-03-06 22:22:25 +0800

    Добавлен вызов обновления на изменение модели

--- src/range-slider/Presenter/Presenter.ts
+++ src/range-slider/Presenter/Presenter.ts
@@ -2,6 +2,8 @@ import type { IOptions } from '../lib/interfaces';
 import type Model from '../Model/Model';
 import type View from '../View/View';
 
+import { Events } from '../lib/constants';
+
 class Presenter {
   private model: Model;
 
@@ -10,7 +12,11 @@ class Presenter {
   constructor(model: Model, view: View) {
     this.model = model;
     this.view = view;
-    console.log(this.view);
+
+    this.model.subscribe(Events.NEW_STATE, (options) =>
+      this.view.update(options)
+    );
+    this.model.emit(Events.NEW_STATE, this.model.getState());
   }
 
   update(options: Partial<IOptions>) {
