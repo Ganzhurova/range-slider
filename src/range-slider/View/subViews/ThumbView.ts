b026aa540f30bdb9f3c202eec4e830936fe3e929
Darima Ganzhurova <DGanzurova@gmail.com>
2022-03-24 15:38:34 +0800

    Переписываю метод движения ползунков

--- src/range-slider/View/subViews/ThumbView.ts
+++ src/range-slider/View/subViews/ThumbView.ts
@@ -1,9 +1,17 @@
 import Component from '../Component';
-// import { Events } from '../../lib/constants';
-import { HTML } from '../../lib/html';
+import { Events } from '../../lib/constants';
+import { HTML, mix } from '../../lib/html';
+import helpers from '../../helpers/helpers';
+import { ILimitCoords } from '../../lib/interfaces';
 
 class ThumbView extends Component {
   private percentPosition!: number;
+
+  private startPxCoord!: number;
+
+  private step = 0;
+
+  private limitCoords!: ILimitCoords;
 
   constructor() {
     super();
@@ -12,7 +20,20 @@ class ThumbView extends Component {
 
   private setPercentPosition(percentPosition: number): void {
     this.percentPosition = percentPosition;
-    // this.emit(Events.NEW_PERCENT_POSITION, this.percentPosition);
+  }
+
+  private setLimitCoords(limitCoords: ILimitCoords): void {
+    this.limitCoords = limitCoords;
+  }
+
+  private getValidCoord(coord: number): number {
+    if (coord < this.limitCoords.start) {
+      return this.limitCoords.start;
+    }
+    if (coord > this.limitCoords.end) {
+      return this.limitCoords.end;
+    }
+    return coord;
   }
 
   public getPercentPosition(): number {
@@ -25,9 +46,75 @@ class ThumbView extends Component {
       'style',
       `${ThumbView.direction.name}:${this.percentPosition}%`
     );
+    this.subscribe(Events.THUMB_SELECTED, (target: HTMLElement) => {
+      if (target === this.el) {
+        this.addClass(mix.selected);
+      } else {
+        this.removeClass(mix.selected);
+      }
+    });
   }
 
-  
+  public handlerThumbDragStart(
+    event: MouseEvent | TouchEvent,
+    limitCoords: ILimitCoords
+  ): void {
+    event.preventDefault();
+
+    const target = (<HTMLElement>event.target).closest(
+      `.${HTML.thumb.className}`
+    );
+    if (target) {
+      this.emit(Events.THUMB_SELECTED, target);
+    }
+    if (target !== this.el) return;
+
+    this.setLimitCoords(limitCoords);
+
+    const eventCoord = helpers.getEventCoord(event);
+    // const thumbCoord = this.getCoord();
+    // const shift = eventCoord - thumbCoord;
+    this.startPxCoord = eventCoord;
+
+    const handlerThumbDrag = this.handlerThumbDrag.bind(this);
+
+    const handlerThumbDragEnd = () => {
+      document.removeEventListener('mousemove', handlerThumbDrag);
+      document.removeEventListener('mouseup', handlerThumbDragEnd);
+      document.removeEventListener('touchmove', handlerThumbDrag);
+      document.removeEventListener('touchend', handlerThumbDragEnd);
+    };
+
+    document.addEventListener('mousemove', handlerThumbDrag);
+    document.addEventListener('mouseup', handlerThumbDragEnd);
+    document.addEventListener('touchmove', handlerThumbDrag);
+    document.addEventListener('touchend', handlerThumbDragEnd);
+  }
+
+  private handlerThumbDrag(event: MouseEvent | TouchEvent) {
+    // const startPxCoord = 0;
+    // console.log(this.startPxCoord);
+    console.log(this.getPercentPosition());
+
+    const endPxCoord = helpers.getEventCoord(event);
+    const delta = (this.startPxCoord - endPxCoord) * Component.percentPerPx;
+    const percentPosition = this.getValidCoord(
+      this.getPercentPosition() - delta
+    );
+    this.setPercentPosition(percentPosition);
+    this.startPxCoord = endPxCoord;
+    console.log(delta);
+
+    // console.log(this.startPxCoord);
+
+    console.log(this.getPercentPosition());
+
+    this.emit(Events.NEW_PERCENT_POSITION, this.percentPosition);
+    this.el.setAttribute(
+      'style',
+      `${ThumbView.direction.name}:${this.percentPosition}%`
+    );
+  }
 }
 
 export default ThumbView;
