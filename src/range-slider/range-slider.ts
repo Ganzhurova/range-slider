c3962c5a31f6f08db7c1028d585827038796a00d
Darima Ganzhurova <DGanzurova@gmail.com>
2022-04-06 10:43:12 +0800

    Добавлена оболочка jQuery

---
+++ src/range-slider/range-slider.ts
@@ -0,0 +1,21 @@
+/* eslint-disable func-names */
+import './range-slider.scss';
+
+import Presenter from './Presenter/Presenter';
+import Model from './Model/Model';
+import View from './View/View';
+import { IOptions } from './lib/interfaces';
+
+(function ($) {
+  // eslint-disable-next-line no-param-reassign
+  $.fn.rangeSlider = function (options?: IOptions) {
+    const key = 'rangeSlider';
+    const settings = options || {};
+
+    return this.each(function () {
+      if (!$.data(this, key)) {
+        $.data(this, key, new Presenter(new Model(settings), new View(this)));
+      }
+    });
+  };
+})(jQuery);
