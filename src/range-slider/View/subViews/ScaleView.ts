694b32438188efbd0ab6bb6d6f5d75e43e0940cd
Darima Ganzhurova <DGanzurova@gmail.com>
2022-04-10 12:25:50 +0800

    Рефакторинг

--- src/range-slider/View/subViews/ScaleView.ts
+++ src/range-slider/View/subViews/ScaleView.ts
@@ -1,39 +1,57 @@
-import type { ObjValue } from '../../lib/types';
-import Component from '../Component';
 import { HTML } from '../../lib/html';
-import { Events } from '../../lib/constants';
+import { Settings } from '../../lib/interfaces';
+import { Html } from '../../lib/types';
+import Component from '../Component';
 
 class ScaleView extends Component {
-  private isElExists = false;
-
   private values: Component[] = [];
 
   private points: Component[] = [];
 
-  constructor() {
-    super();
-    this.init(HTML.scale);
+  private getScaleValues(): string[] {
+    const values: string[] = [];
+    const { min, max, scaleParts } = this.options;
+    const step: number = (max - min) / scaleParts;
+
+    let value: number = min;
+
+    for (let i = 0; i <= scaleParts; i += 1) {
+      values.push(value.toFixed(this.data.fractionLength));
+      value = +value + step;
+    }
+
+    return values;
+  }
+
+  private createComponent(html: Html, text?: string): Component {
+    const settings: Settings = {
+      data: this.data,
+      options: this.options,
+    };
+    const component = new Component(html, settings);
+    if (text) {
+      component.getEl().textContent = text;
+    }
+
+    return component;
   }
 
   private renderComponent(
-    html: ObjValue<typeof HTML>,
-    percent: number,
-    text: string,
-    array: Component[]
+    html: Html,
+    percentPosition: number,
+    array: Component[],
+    text?: string
   ): void {
-    const component = new Component();
-    component.init(html);
+    const component = this.createComponent(html, text);
 
-    const el = component.getEl();
-    if (text) {
-      el.textContent = text;
-    }
-    el.setAttribute(
-      'style',
-      `${
-        ScaleView.direction.name
-      }:${percent}%; transform: translate${ScaleView.direction.coord.toUpperCase()}(-50%)`
-    );
+    component
+      .getEl()
+      .setAttribute(
+        'style',
+        `${
+          this.data.direction.name
+        }:${percentPosition}%; transform: translate${this.data.direction.coord.toUpperCase()}(-50%)`
+      );
 
     this.addChild(component);
     array.push(component);
@@ -43,9 +61,7 @@ class ScaleView extends Component {
     let currIndex = 0;
     let nextIndex = 1;
 
-    for (let i = currIndex; i < this.values.length; i += 1) {
-      if (currIndex === this.values.length - 1) break;
-
+    for (let i = currIndex; i < this.values.length - 1; i += 1) {
       const isOverlay = ScaleView.checkOverlay(
         this.values[currIndex],
         this.values[nextIndex]
@@ -53,8 +69,50 @@ class ScaleView extends Component {
 
       if (isOverlay) {
         if (nextIndex === this.values.length - 1) {
-          this.values[currIndex].hidden();
-          this.points[currIndex].hidden();
+          const penultValue = this.values[currIndex];
+          const lastValue = this.values[nextIndex];
+          const offsetPX =
+            penultValue.getCoord() +
+            penultValue.getSize() -
+            lastValue.getCoord();
+          const isLessOneThird = () => offsetPX < penultValue.getSize() / 3;
+
+          if (isLessOneThird()) {
+            const offsetPercent = (offsetPX * 100) / lastValue.getSize();
+            const transformPercent = -50 + offsetPercent;
+
+            lastValue.getEl().style.transform = `translate${this.data.direction.coord.toUpperCase()}(${transformPercent}%)`;
+            console.log(transformPercent);
+          } else {
+            this.values[currIndex].hidden();
+            this.points[currIndex].hidden();
+          }
+
+          // const direction = <'left' | 'top'>this.data.direction.name;
+          // const lastValue = this.values[nextIndex];
+          // const penultValue = this.values[currIndex];
+          // const penultPosition = parseInt(
+          //   penultValue.getEl().style[direction],
+          //   10
+          // );
+          // const penultSize = this.pxToPercent(penultValue.getSize());
+
+          // if (penultPosition + penultSize > 100) {
+          //   const offsetPx = this.percentToPx(
+          //     penultPosition + penultSize - 100
+          //   );
+          //   const offsetPercent = (100 * offsetPx) / lastValue.getSize();
+          //   console.log(offsetPercent);
+
+          //   lastValue.getEl().style.transform = `translate${this.data.direction.coord.toUpperCase()}(${
+          //     -50 + offsetPercent
+          //   }%)`;
+          // }
+
+          // const lastValuePosition = this.values[currIndex].getEl().style.left;
+
+          // this.values[nextIndex].show();
+          // this.points[nextIndex].show();
         } else {
           this.values[nextIndex].hidden();
           this.points[nextIndex].hidden();
@@ -69,40 +127,30 @@ class ScaleView extends Component {
     }
   }
 
-  public setIsElExists(isElExists: boolean): void {
-    this.isElExists = isElExists;
-  }
-
-  public renderDivisions(textValues: string[]): void {
+  public renderDivisions(): void {
     if (!this.isElExists) return;
     this.el.innerHTML = '';
     this.values = [];
     this.points = [];
 
+    const textValues = this.getScaleValues();
     const step = 100 / (textValues.length - 1);
 
     textValues.forEach((text, i) => {
-      this.renderComponent(HTML.scalePoint, step * i, '', this.points);
-      this.renderComponent(HTML.scaleValue, step * i, text, this.values);
+      this.renderComponent(HTML.scalePoint, step * i, this.points);
+      this.renderComponent(HTML.scaleValue, step * i, this.values, text);
     });
   }
 
-  public setup(lineSize: number): void {
+  public setup(): void {
     if (!this.isElExists) return;
 
-    this.el.setAttribute('style', `${ScaleView.direction.size}:${lineSize}%`);
+    this.el.setAttribute(
+      'style',
+      `${this.data.direction.size}:${this.data.percentageLimitSize}%`
+    );
+
     this.setVisibilityOfValues();
-  }
-
-  public handlerScaleValueClick(event: MouseEvent | TouchEvent) {
-    event.preventDefault();
-
-    const target = <HTMLElement>event.target;
-    if (!target.closest(`.${HTML.scaleValue.className}`)) return;
-
-    const index = this.values.findIndex((value) => value.getEl() === target);
-
-    this.emit(Events.NEW_PERCENT_POSITION, index);
   }
 }
 
