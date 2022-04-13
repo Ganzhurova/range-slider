ea21e16b8b82bf2e989ee00beadd76db77d3fda3
Darima Ganzhurova <DGanzurova@gmail.com>
2022-04-13 15:39:09 +0800

    Add callbacks onStart and onChange

--- src/range-slider/Presenter/Presenter.ts
+++ src/range-slider/Presenter/Presenter.ts
@@ -14,14 +14,40 @@ class Presenter {
     this.model = new Model();
     this.view = new View(element, this.model.getState());
 
-    this.model.subscribe(Events.NEW_STATE, (keys: OptionsKeys[]) =>
-      this.view.update(keys)
-    );
+    this.init(options);
     this.update(options);
   }
 
-  update(options?: Partial<IOptions>) {
-    this.model.updateState(options);
+  private init(options?: Partial<IOptions>): void {
+    this.model.subscribe(Events.NEW_STATE, (keys: OptionsKeys[]) =>
+      this.view.update(keys)
+    );
+    this.view.subscribe(Events.NEW_POSITION, () => {
+      this.callOnChange(options);
+    });
+  }
+
+  private callOnStart(options?: Partial<IOptions>): void {
+    if (!options?.onStart) return;
+    options.onStart(this.model.getState());
+  }
+
+  private callOnChange(options?: Partial<IOptions>): void {
+    if (!options?.onChange) return;
+    options.onChange(this.model.getState());
+  }
+
+  public update(options?: Partial<IOptions>): void {
+    if (!options) return;
+
+    const getStateOptions = ({
+      onStart,
+      onChange,
+      ...state
+    }: Partial<IOptions>) => state;
+
+    this.model.updateState(getStateOptions(options));
+    this.callOnStart(options);
   }
 }
 
