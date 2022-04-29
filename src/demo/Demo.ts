a5c4307bbd28bcb85908708123b5635b0f1a9647
Darima Ganzhurova <DGanzurova@gmail.com>
2022-04-29 08:55:21 +0800

    Добавлена обработка событий ввода значений и выбора

--- src/demo/Demo.ts
+++ src/demo/Demo.ts
@@ -12,6 +12,7 @@ class Demo {
     this.instance = sliderInstance;
 
     this.initPanel(index);
+    this.addEventListeners();
     this.bindValues();
   }
 
@@ -27,23 +28,62 @@ class Demo {
     this.inputs = <HTMLInputElement[]>[...this.panel.elements];
   }
 
+  private addEventListeners() {
+    this.inputs.forEach((input) => {
+      const key = input.name;
+
+      input.addEventListener('change', () => {
+        if (input.type === 'checkbox') {
+          this.instance.update({ [key]: input.checked });
+
+          if (key === 'isDouble') {
+            this.blockToField(input.checked);
+          }
+        } else {
+          const value = +input.value;
+          this.instance.update({ [key]: value });
+        }
+      });
+    });
+  }
+
   private bindValues() {
+    const { panel } = this;
     const { inputs } = this;
+    const blockToField = this.blockToField.bind(this);
 
     this.instance.update({
       onStart(data) {
-        for (let i = 0; i < inputs.length; i += 1) {
-          const input = inputs[i];
+        inputs.forEach((item) => {
+          const input = item;
           const key = <keyof typeof data>input.name;
 
           if (input.type === 'checkbox') {
             input.checked = <boolean>data[key];
+
+            if (key === 'isDouble') {
+              blockToField(input.checked);
+            }
           } else {
             input.value = String(<number>data[key]);
           }
-        }
+        });
+      },
+      onChange(data) {
+        panel.from.value = data.from;
+        panel.to.value = data.to;
       },
     });
+  }
+
+  private blockToField(isDouble: boolean) {
+    this.panel.to.disabled = !isDouble;
+
+    if (isDouble) {
+      this.panel.to.style.color = '';
+    } else {
+      this.panel.to.style.color = 'transparent';
+    }
   }
 }
 
